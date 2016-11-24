ca90e38fe1fee080b84354784d9f93eb97e51cf3
Justin Lim <justlim77@gmail.com>
2016-11-24 18:00:51 +0800

    Add threaded job class

---
+++ Assets/Scripts/Core/ThreadedJob.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThreadedJob
+{
+    private bool m_isDone = false;
+    private object m_handle = new object();
+    private System.Threading.Thread m_thread = null;
+
+    public bool IsDone
+    {
+        get
+        {
+            bool temp;
+            lock (m_handle)
+            {
+                temp = m_isDone;
+            }
+            return temp;
+        }
+        set
+        {
+            lock (m_handle)
+            {
+                m_isDone = value;
+            }
+        }
+    }
+
+    public virtual void Start()
+    {
+        m_thread = new System.Threading.Thread(Run);
+        m_thread.Start();
+    }
+
+    public virtual void Abort()
+    {
+        m_thread.Abort();
+    }
+
+    protected virtual void ThreadedFunction() { }
+    protected virtual void OnFinished() { }
+
+    public virtual bool Update()
+    {
+        if (IsDone)
+        {
+            OnFinished();
+            return true;
+        }
+        return false;
+    }
+
+    public IEnumerator WaitFor()
+    {
+        while (!Update())
+        {
+            yield return null;
+        }
+    }
+
+    private void Run()
+    {
+        ThreadedFunction();
+        IsDone = true;
+    }
+}
